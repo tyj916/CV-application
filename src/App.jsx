ab6296660490eded6fc5bd4f1b4d40ae368347a1
Tan Yi Jun <tanyijun6@gmail.com>
2024-07-25 23:45:40 +0800

    Initialize cv form

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,147 @@
+import { useState } from 'react';
 import './App.css'
-import Education from './Education'
-import General from './General'
-import Practical from './Practical'
+import { educationList, generalInfo, practicalExp } from './data';
 
 function generateCV(e) {
   e.preventDefault();
   return false;
 }
 
-function App() {
+function General() {
+  const {name, email, phoneNumber} = generalInfo;
+
   return (
     <>
+      <label htmlFor="name">Name: </label>
+      <input type="text" id="name" required defaultValue={name}/>
+
+      <label htmlFor="email">Email: </label>
+      <input type="email" id="email" defaultValue={email}/>
+
+      <label htmlFor="phone-number">Phone number: </label>
+      <input type="tel" id="phone-number" defaultValue={phoneNumber}/>
+    </>
+  )
+}
+
+function EducationInfo({schoolName, degree, fieldOfStudy, dateStart, dateEnd}) {
+  return (
+    <>
+      <label htmlFor="school-name">School name: </label>
+      <input type="text" id="school-name" required defaultValue={schoolName} />
+
+      <label htmlFor="degree">Degree: </label>
+      <input type="text" id="degree" defaultValue={degree} />
+
+      <label htmlFor="field-of-study">Field of study: </label>
+      <input type="text" id="field-of-study" defaultValue={fieldOfStudy} />
+
+      <label htmlFor="date-start">Date start:</label>
+      <input type="date" id="date-start" defaultValue={dateStart} />
+
+      <label htmlFor="date-end">Date end:</label>
+      <input type="date" id="date-end" defaultValue={dateEnd} />
+    </>
+  )
+}
+
+function EducationList() {
+  return (
+    <>
+      {
+        educationList.map((education, index) => {
+          return <EducationInfo 
+            key={index}
+            {...education}
+            onChange={()=>{}}
+          />
+        })
+      }
+    </>
+  );
+}
+
+function Education() {
+
+  return (
+    <>
+      <EducationList />
+    </>
+  )
+}
+
+function PracticalInfo({companyName, title, responsibility, dateStart, dateEnd}) {
+  return (
+    <>
+      <label htmlFor="company-name">Company name: </label>
+      <input type="text" id="company-name" required defaultValue={companyName}/>
+
+      <label htmlFor="position-title">Position title: </label>
+      <input type="text" id="position-title" defaultValue={title} />
+
+      <label htmlFor="main-responsibilities">Main responsibilities: </label>
+      <textarea name="main-responsibilities" id="main-responsibilities" defaultValue={responsibility}></textarea>
+
+      <label htmlFor="date-start">Date start:</label>
+      <input type="date" id="date-start" defaultValue={dateStart} />
+
+      <label htmlFor="date-end">Date end:</label>
+      <input type="date" id="date-end" defaultValue={dateEnd} />
+    </>
+  )
+}
+
+function PracticalLists() {
+  return (
+    <>
+      {
+        practicalExp.map((info, index) => {
+          return (
+            <PracticalInfo 
+              key={index}
+              {...info}
+            />
+          )
+        })
+      }
+    </>
+  );
+}
+
+function Practical() {
+  return (
+    <>
+      <PracticalLists />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main className='main-container'>
       <form onSubmit={generateCV}>
-        <General />
-        <hr></hr>
-        <Education />
-        <hr></hr>
-        <Practical />
-        <hr></hr>
+        <fieldset className="general-info">
+          <legend>General Information</legend>
+          <General />
+        </fieldset>
+
+        <fieldset className="education-exp">
+          <legend>Education Experience</legend>
+          <Education />
+        </fieldset>
+
+        <fieldset className="practical-exp">
+          <legend>Practical Experience</legend>
+          <Practical />
+        </fieldset>
+
         <button type="submit">Generate</button>
       </form>
-    </>
+
+      <div className="result">
+        result
+      </div>
+    </main>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const educationList = [
+  {
+    schoolName: 'ABC School',
+    degree: 'Bachelor of Information Technology',
+    fieldOfStudy: 'Cyber Security and Forensics and Computer Science',
+    dateStart: '2018-07-01',
+    dateEnd: '2020-03-18',
+  },
+  {
+    schoolName: 'DEF School',
+    degree: 'Bachelor of Information Technology',
+    fieldOfStudy: 'Cyber Security and Forensics and Computer Science',
+    dateStart: '2018-07-01',
+    dateEnd: '2020-03-18',
+  },
+];
+
+export const generalInfo = {
+  name: 'Mr ABC',
+  email: 'example@email.com',
+  phoneNumber: '0000000000',
+};
+
+export const practicalExp = [
+  {
+    companyName: 'ABC Company',
+    title: 'E-Commerce Executive',
+    responsibility: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus pariatur, fugiat quo dolorem maiores minima assumenda delectus consequatur consectetur placeat necessitatibus repudiandae dolores accusamus saepe eos aliquid, exercitationem incidunt expedita.',
+    dateStart: '2021-03-01',
+    dateEnd: '2023-01-31',
+  },
+  {
+    companyName: 'ABC Company',
+    title: 'Web Developer',
+    responsibility: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus pariatur, fugiat quo dolorem maiores minima assumenda delectus consequatur consectetur placeat necessitatibus repudiandae dolores accusamus saepe eos aliquid, exercitationem incidunt expedita.',
+    dateStart: '2020-03-01',
+    dateEnd: '2021-02-28',
+  },
+]
